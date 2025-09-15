4e3049e28ce662efdaba6d9a832af64cd3dea09b
Arun <arunneupane0000@gmail.com>
2025-09-15 13:36:34 +0545

    feat : general component completed

--- src/Components/Setting/General.jsx
+++ src/Components/Setting/General.jsx
@@ -5,6 +5,7 @@ import { CiGlobe } from "react-icons/ci";
 
 const General = () => {
   const [startAtLogin, setStartAtLogin] = useState(false);
+  const [emojiReplace, setEmojiReplace] = useState(true);
 
   return (
     <div className="flex flex-col gap-4">
@@ -51,6 +52,53 @@ const General = () => {
           <option value="Nepali">Nepali</option>
         </select>
       </div>
+      <h2 className="text-xl">Typing</h2>
+      <p className="text-[12px] text-gray-300">
+        Change typing settings for <strong>autocorrect</strong> and{" "}
+        <strong>misspelled highlight</strong> from{" "}
+        <a
+          className="text-green-500 border-b-1 border-green cursor-pointer hover:border-0"
+          href="#"
+        >
+          Windows Settings.
+        </a>
+      </p>
+      <h2 className="text-md">Replace text with emoji</h2>
+      <div className="flex items-center justify-between">
+        <p className="text-[12px] text-gray-300 w-[180px] text-wrap">
+          Emoji will replace specific text as you type
+        </p>
+        <div className="flex items-center gap-3">
+          <span>😁</span>
+          {emojiReplace && (
+            <div
+              className="flex gap-2 text-green-500 items-center"
+              onClick={() => setEmojiReplace(!emojiReplace)}
+            >
+              <IoToggleSharp style={{ fontSize: "25px", cursor: "pointer" }} />
+            </div>
+          )}
+
+          {!emojiReplace && (
+            <div
+              className="flex gap-2 text-gray-200 items-center"
+              onClick={() => setEmojiReplace(!emojiReplace)}
+            >
+              <FaToggleOff style={{ fontSize: "25px", cursor: "pointer" }} />
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="w-full border-b-1 border-white/30"></div>
+      <p className="text-[12px] text-gray-300">
+        To log out of ArunApp on this computer go to your{" "}
+        <a
+          href="#"
+          className="text-green-500 border-b-1 border-green cursor-pointer hover:border-0"
+        >
+          Profile
+        </a>
+      </p>
     </div>
   );
 };
