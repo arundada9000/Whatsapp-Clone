22663c2a74a5bfa2cec83770bdb50778cab5a7fc
Arun <arunneupane0000@gmail.com>
2025-11-07 17:06:30 +0545

    readded _redirects

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
+import fs from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [
+    react(),
+    tailwindcss(),
+    {
+      name: "copy-redirects",
+      writeBundle() {
+        fs.copyFileSync("_redirects", "dist/_redirects");
+      },
+    },
+  ],
+});
